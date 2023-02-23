7a216f059d78c03f25ab3f6aa45b924e1ddc34de
PrivatePresley <nathan_townes_anderson@hotmail.com>
2023-02-23 11:58:24 -0500

    Homework complete

--- app/js/index.js
+++ app/js/index.js
@@ -29,9 +29,32 @@ el.innerHTML = recipeName;
 
 var modal = document.querySelector(".modal");
 var modalOuter = document.querySelector(".modal-outer");
+var modalContent = document.querySelector(".modal p");
+
+const betaOrig = modal.querySelector("p").innerText;
+const pickIt = "You clicked the Pick It button!";
+const cookIt = "You clicked the Cook It button!";
+const storeIt = "You clicked the Store It button!";
 
 function showPopover(event) {
-  if (!event.target.matches(".beta, .closer")) return;
+  if (
+    !event.target.matches(
+      ".beta, .closer, .nav-pickit a, .nav-cookit a, .nav-storeit a"
+    )
+  ) {
+    console.log(event.target);
+    return;
+  }
+  if (event.target.matches(".nav-pickit a")) {
+    modalContent.innerText = pickIt;
+  } else if (event.target.matches(".nav-cookit a")) {
+    modalContent.innerText = cookIt;
+  } else if (event.target.matches(".nav-storeit a")) {
+    modalContent.innerText = storeIt;
+  } else {
+    modalContent.innerText = betaOrig;
+  }
+
   modalOuter.classList.toggle("open");
   event.preventDefault();
 }
