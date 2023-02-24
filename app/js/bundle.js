c7998b2b833de0bab298c84d94fe1629e9afd56a
PrivatePresley <nathan_townes_anderson@hotmail.com>
2023-02-23 20:15:53 -0500

    Added in button JS

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -1,5 +1,5 @@
 (function () {
-  "use strict";
+  'use strict';
 
   function random(array) {
     const max = array.length;
@@ -7,12 +7,29 @@
     return array[randomIndex];
   }
 
-  document.querySelector(".modal");
   var modalOuter = document.querySelector(".modal-outer");
+  var modalInner = document.querySelector(".modal");
+
+  var betaContent = `
+<h3>Oooops!</h3>
+<p>Wow! Nothing works!<p>
+`;
+  var buttonContent = `
+<h2>Coming Soon</h2>
+<p>This feature coming soon.<p>
+<a class="closer" href="#0">✖︎</a>
+`;
 
   function showPopover(event) {
-    if (!event.target.matches(".beta, .closer")) return;
-    modalOuter.classList.toggle("open");
+    if (event.target.matches(".beta")) {
+      modalInner.innerHTML = betaContent;
+      modalOuter.classList.add("open");
+    } else if (event.target.closest("nav ul")) {
+      modalInner.innerHTML = buttonContent;
+      modalOuter.classList.add("open");
+    } else if (event.target.matches(".closer, .modal-outer")) {
+      modalOuter.classList.remove("open");
+    } else return;
     event.preventDefault();
   }
 
@@ -37,4 +54,7 @@
   el.innerHTML = recipeName;
 
   document.addEventListener("click", showPopover);
+
+  console.log("ELEMENT:" + el);
+
 })();
